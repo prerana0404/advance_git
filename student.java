6053796c99459ae615a361629ade3aa6e4c10685
prerana girame <preranagirame2004@gmail.com>
2026-01-04 12:42:23 +0530

    print age value

--- student.java
+++ student.java
@@ -2,6 +2,8 @@ public class student {
 public static void main(String[] args) {
     int age=10;
     String name="John";
+    System.out.println("Name: " + name + ", Age: " + age);
+    System.out.println("Hello World");
 }
 
 }
