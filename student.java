5acda07f8c9828bc340fc23933c0c565b9dd0439
prerana girame <preranagirame2004@gmail.com>
2026-01-06 10:36:16 +0530

    revert

--- student.java
+++ student.java
@@ -2,8 +2,6 @@ public class student {
 public static void main(String[] args) {
     int age=10;
     String name="John";
-    System.out.println("Name: " + name + ", Age: " + age);
-    System.out.println("Hello World");
 }
 
 }
